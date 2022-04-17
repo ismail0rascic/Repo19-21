726c237075253872ba42c61b47002208ce3c68dd
“ismail0rascic” <ismail0rascic.gmail.com>
2022-04-17 23:21:06 +0200

    index

---
+++ src/index.js
@@ -0,0 +1,398 @@
+let caps_val = false
+
+
+function one() {
+    var x = document.getElementById("one").value;
+    document.getElementById("textarea").value += x
+
+}
+
+function two() {
+    var x = document.getElementById("two").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function three() {
+    var x = document.getElementById("three").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function four() {
+    var x = document.getElementById("four").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function five() {
+    var x = document.getElementById("five").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function six() {
+    var x = document.getElementById("six").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function seven() {
+    var x = document.getElementById("seven").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function eight() {
+    var x = document.getElementById("eight").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function nine() {
+    var x = document.getElementById("nine").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function zero() {
+    var x = document.getElementById("zero").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function q() {
+    var x = document.getElementById("q").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+        shift_val = false;
+
+    } else { document.getElementById("textarea").value += x; }
+
+
+}
+
+function w() {
+    var x = document.getElementById("w").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+
+}
+
+function e() {
+    var x = document.getElementById("e").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+
+}
+
+function r() {
+    var x = document.getElementById("r").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function t() {
+    var x = document.getElementById("t").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function z() {
+    var x = document.getElementById("z").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function u() {
+    var x = document.getElementById("u").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function i() {
+    var x = document.getElementById("i").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function o() {
+    var x = document.getElementById("o").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function p() {
+    var x = document.getElementById("p").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function a() {
+    var x = document.getElementById("a").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function s() {
+    var x = document.getElementById("s").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function d() {
+    var x = document.getElementById("d").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function f() {
+    var x = document.getElementById("f").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function g() {
+    var x = document.getElementById("g").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function h() {
+    var x = document.getElementById("h").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function j() {
+    var x = document.getElementById("k").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function k() {
+    var x = document.getElementById("k").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function l() {
+    var x = document.getElementById("l").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function y() {
+    var x = document.getElementById("y").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function x() {
+    var x = document.getElementById("x").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function c() {
+    var x = document.getElementById("c").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function v() {
+    var x = document.getElementById("v").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function b() {
+    var x = document.getElementById("b").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function n() {
+    var x = document.getElementById("n").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else { document.getElementById("textarea").value += x; }
+
+}
+
+function m() {
+    var x = document.getElementById("m").value;
+    if (caps_val == true) {
+        document.getElementById("textarea").value += x.toUpperCase();
+    } else {
+        document.getElementById("textarea").value += x;
+
+    }
+}
+
+function point() {
+    var x = document.getElementById("point").value;
+
+    document.getElementById("textarea").value += x
+
+}
+
+function dash() {
+    var x = document.getElementById("dash").value;
+
+    document.getElementById("textarea").value += x
+}
+
+
+function submit() {
+    alert(document.getElementById("textarea").value);
+}
+
+function backSpace() {
+
+
+    document.getElementById("textarea").value = document.getElementById("textarea").value.substring(0, document.getElementById("textarea").value.length - 1);
+}
+
+
+function space() {
+    let x = document.getElementById("space").value;
+
+    document.getElementById("textarea").value += x
+
+
+}
+
+function shift() {}
+
+function enter() {}
+
+function capsLock() {
+
+    if (caps_val == false) {
+        caps_val = true
+        document.getElementById("q").innerHTML = document.getElementById("q").value.toUpperCase();
+        document.getElementById("w").innerHTML = document.getElementById("w").value.toUpperCase();
+        document.getElementById("e").innerHTML = document.getElementById("e").value.toUpperCase();
+        document.getElementById("r").innerHTML = document.getElementById("r").value.toUpperCase();
+        document.getElementById("t").innerHTML = document.getElementById("t").value.toUpperCase();
+        document.getElementById("y").innerHTML = document.getElementById("y").value.toUpperCase();
+        document.getElementById("u").innerHTML = document.getElementById("u").value.toUpperCase();
+        document.getElementById("i").innerHTML = document.getElementById("i").value.toUpperCase();
+        document.getElementById("o").innerHTML = document.getElementById("o").value.toUpperCase();
+        document.getElementById("p").innerHTML = document.getElementById("p").value.toUpperCase();
+        document.getElementById("a").innerHTML = document.getElementById("a").value.toUpperCase();
+        document.getElementById("s").innerHTML = document.getElementById("s").value.toUpperCase();
+        document.getElementById("d").innerHTML = document.getElementById("d").value.toUpperCase();
+        document.getElementById("f").innerHTML = document.getElementById("f").value.toUpperCase();
+        document.getElementById("g").innerHTML = document.getElementById("g").value.toUpperCase();
+        document.getElementById("h").innerHTML = document.getElementById("h").value.toUpperCase();
+        document.getElementById("j").innerHTML = document.getElementById("j").value.toUpperCase();
+        document.getElementById("k").innerHTML = document.getElementById("k").value.toUpperCase();
+        document.getElementById("l").innerHTML = document.getElementById("l").value.toUpperCase();
+        document.getElementById("z").innerHTML = document.getElementById("z").value.toUpperCase();
+        document.getElementById("x").innerHTML = document.getElementById("x").value.toUpperCase();
+        document.getElementById("c").innerHTML = document.getElementById("c").value.toUpperCase();
+        document.getElementById("v").innerHTML = document.getElementById("v").value.toUpperCase();
+        document.getElementById("b").innerHTML = document.getElementById("b").value.toUpperCase();
+        document.getElementById("n").innerHTML = document.getElementById("n").value.toUpperCase();
+        document.getElementById("m").innerHTML = document.getElementById("m").value.toUpperCase();;
+
+
+    } else {
+        caps_val = false
+        document.getElementById("q").innerHTML = "q"
+        document.getElementById("w").innerHTML = "w"
+        document.getElementById("e").innerHTML = "e"
+        document.getElementById("r").innerHTML = "r"
+        document.getElementById("t").innerHTML = "t"
+        document.getElementById("y").innerHTML = "y"
+        document.getElementById("u").innerHTML = "u"
+        document.getElementById("i").innerHTML = "i"
+        document.getElementById("o").innerHTML = "o"
+        document.getElementById("p").innerHTML = "p"
+        document.getElementById("a").innerHTML = "a"
+        document.getElementById("s").innerHTML = "s"
+        document.getElementById("d").innerHTML = "d"
+        document.getElementById("f").innerHTML = "f"
+        document.getElementById("g").innerHTML = "g"
+        document.getElementById("h").innerHTML = "h"
+        document.getElementById("j").innerHTML = "j"
+        document.getElementById("k").innerHTML = "k"
+        document.getElementById("l").innerHTML = "l"
+        document.getElementById("z").innerHTML = "z"
+        document.getElementById("x").innerHTML = "x"
+        document.getElementById("c").innerHTML = "c"
+        document.getElementById("v").innerHTML = "v"
+        document.getElementById("b").innerHTML = "b"
+        document.getElementById("n").innerHTML = "n"
+        document.getElementById("m").innerHTML = "m"
+
+    }
+
+
+}
